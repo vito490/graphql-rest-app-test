86564dae5164618f5d9ea622d6fb7be00b4a1f46
059298758 <vito.sammarco1@ibm.com>
2023-07-10 17:51:07 +0200

    first postman collection tests

--- Logical/GrapQL-REST-demo-app.postman_collection.scm/GrapQL-REST-demo-app.scm/Book use case.scm/config.scm
+++ Logical/GrapQL-REST-demo-app.postman_collection.scm/GrapQL-REST-demo-app.scm/Book use case.scm/config.scm
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<editableResource createdTimestamp="1688993806285"
+<editableResource createdTimestamp="1689004034455"
     createdUser="059298758"
-    id="-714a168c:1893fd66f9d:-7ec2:-1356061446:-1834518822"
-    type="service_component_resource" updatedTimestamp="1688993806285"
+    id="2b0535b5:1894078ddb5:-7df0:-1356061446:-1834518822"
+    type="service_component_resource" updatedTimestamp="1689004034455"
     updatedUser="059298758" version="1.1">
     <documentation>
         <description externalID="5057002a-703b-33d2-a148-d1a41733c3ba" value="Posizione del file:C:\progetti\demo\Urban Code\comprehensive-demo\rational\GrapQL-REST-demo-app.postman_collection.json&#xd;&#xa;"/>
